b4a40a504a55c16a4e61d1bd7fb9f5ec6835fd6c
GiovanniCasini <giovanni.casini1@stud.unifi.it>
2022-01-18 11:40:09 +0100

    added observer

---
+++ source/observer.h
@@ -0,0 +1,21 @@
+#include <wx/grid.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+
+class Observer{
+    public:
+        void Update(wxGrid a, wxGrid b){
+            auto size = a.GetSize();
+            float sum = 0;
+            for(int i=0;i<size.GetWidth();i++){
+                for(int j=0;j<size.GetHeight();j++){
+                    if(a.GetCellValue(i,j) != "")
+                        sum += wxAtof(a.GetCellValue(i,j));
+                }
+
+            }
+            b.SetCellValue(0,0,std::to_string(sum));
+        }
+}; //..
